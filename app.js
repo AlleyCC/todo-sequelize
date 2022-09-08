72c55bace7c8cef32d96d702a2365254c4d597a7
AlleyChang <yifanchang0602@gmail.com>
2022-09-08 11:37:10 +0800

    feat: add routes and views of login & register

--- app.js
+++ app.js
@@ -9,8 +9,27 @@ app.engine('hbs', engine( {defaultLayout: 'main', extname: '.hbs'}))
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+//home
 app.get('/', (req, res) => {
   res.send('hello world')
+})
+
+//login
+app.get('/users/login', (req, res)=> {
+  res.render('login')
+})
+
+app.post('/users/login', (req, res)=> {
+  res.send('sending login request.')
+})
+
+//register
+app.get('/users/register', (req, res)=> {
+  res.render('register')
+})
+
+app.post('/users/login', (req, res)=> {
+  res.send('sending register request.')
 })
 
 app.listen(PORT, () => {
